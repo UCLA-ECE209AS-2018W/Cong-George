5c2746ef15514e51791fde467d43386093801d54
lonelygoddady <lzqgeorge@hotmail.com>
2018-02-26 07:56:18 +0000

    tcpdump parsing

--- tcpdump packets.py
+++ tcpdump packets.py
@@ -5,11 +5,8 @@ monitor_card = 'wlan1'
 ap_mac = '08:95:2a:6e:18:b8'
 devices_data = {}
 devices_null = {}
-total_data = 0
-total_null = 0
-total_devices = 0
 pack_s = '1000'
-timeout = '60'
+timeout = '600'
 filter_rule1 = ' and type data and not type data subtype null'
 filter_rule2 = ' and not type mgt subtype beacon'
 filter_rule3 = ' type data'
@@ -26,7 +23,7 @@ os.system('timeout ' + timeout + ' tcpdump -i ' + monitor_card +
           ' -s ' + pack_s + ' -w ' + file_name + ' -v' + 
           filter_rule3)
 scapy_pack = rdpcap(file_name)
-scapy_pack.summary()
+#scapy_pack.summary()
 
 #addr1 -> RA, addr2 -> TA, addr3 -> SA
 for packets in scapy_pack:
@@ -55,16 +52,17 @@ for packets in scapy_pack:
 total_data = sum(devices_data.values())
 total_null = sum(devices_null.values())
 
-total_devices = sum(devices_data.keys())
-for device in total_null:
-    if device not in total_data:
+# count the number of devices
+total_devices = len(devices_data)
+for device in devices_null:
+    if device not in devices_data:
         total_devices += 1
 
-print("Null data pack count: " + total_null)
+print("Null data pack count: " + str(total_null))
 print(devices_null)
 
-print("data pack count: " + total_data)
+print("data pack count: " + str(total_data))
 print(devices_data)
 
 print("SGW MAC address: " + SGW_mac)
-print("device number: " + total_devices)
+print("device number: " + str(total_devices))
