97b5083a48ea0db3884b40d137c02ffcea8eb7a5
George Li <lzqgeorge@hotmail.com>
2018-03-15 17:28:40 -0700

    Update database_creation

--- main/database_creation.py
+++ main/database_creation.py
@@ -1,3 +1,8 @@
 from all_functions import *
 
 if __init__ == "__main__":
+  db_file = "signature_database.p"
+  pcap_dir_path = ""
+  
+  # create database and save it in "db_file"
+  create_database(db_file, pcap_dir_path)
